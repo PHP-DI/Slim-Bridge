57c8beb37750e2a7fca003a672ac749ab75a6d1e
Matthieu Napoli <matthieu@mnapoli.fr>
2019-08-05 23:13:34 +0200

    Rename and fix the main class

--- src/Bridge.php
+++ src/Bridge.php
@@ -1,13 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace DI\Bridge\Slim;
 
+use DI\Container;
 use Invoker\Invoker;
 use Invoker\ParameterResolver\AssociativeArrayResolver;
 use Invoker\ParameterResolver\Container\TypeHintContainerResolver;
 use Invoker\ParameterResolver\DefaultValueResolver;
 use Invoker\ParameterResolver\ResolverChain;
 use Psr\Container\ContainerInterface;
+use Slim\App;
 use Slim\Factory\AppFactory;
 use \Invoker\CallableResolver as InvokerCallableResolver;
 
@@ -17,10 +19,12 @@ use \Invoker\CallableResolver as InvokerCallableResolver;
  * As you can see, this class is very basic and is only useful to get started quickly.
  * You can also very well *not* use it and build the container manually.
  */
-class App
+class Bridge
 {
-    public static function boot(ContainerInterface $container)
+    public static function create(ContainerInterface $container = null): App
     {
+        $container = $container ?: new Container;
+
         AppFactory::setContainer($container);
         $callableResolver = new InvokerCallableResolver($container);
         AppFactory::setCallableResolver(new CallableResolver($callableResolver));

16cb0611bdc0551c2bdd5cc4150d3603f4a1f1e8
Ashura <the.lucas2016@gmail.com>
2020-01-13 12:01:18 -0200

    Automatically add Slim\App instance to Container

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -5,6 +5,9 @@ namespace DI\Bridge\Slim\Test;
 use DI\Bridge\Slim\Bridge;
 use DI\Bridge\Slim\Test\Mock\RequestFactory;
 use PHPUnit\Framework\TestCase;
+use DI\ContainerBuilder;
+use Slim\App;
+use Psr\Http\Message\ResponseInterface;
 
 class ApplicationTest extends TestCase
 {
@@ -23,5 +26,27 @@ class ApplicationTest extends TestCase
         $app->handle(RequestFactory::create());
 
         $this->assertTrue($called);
+    }
+
+    /**
+     * @test
+     */
+    public function register_app_instance_to_container()
+    {
+        $app = Bridge::create();
+
+        $instance = null;
+
+        $app->get('/', function (App $app, ResponseInterface $response) use(&$instance) {
+            $instance = $app;
+            return $response;
+        });
+
+        $app->handle(RequestFactory::create());
+
+        $this->assertInstanceOf(
+            App::class,
+            $instance
+        );
     }
 }

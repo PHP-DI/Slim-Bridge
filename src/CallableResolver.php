a7e75c00da72d82207c3335a9d9b4f7f8bd6997a
Danae Dekker <info@danaedekker.com>
2021-04-17 18:02:12 +0200

    Added support for PSR-15 middleware resolving using \Slim\CallableResolver and \Slim\Interfaced\AdvancedCallableResolverInterface
    Fixes #51 and uses code supplied by kakenbok in https://github.com/PHP-DI/Slim-Bridge/issues/51#issuecomment-820936631

--- src/CallableResolver.php
+++ src/CallableResolver.php
@@ -2,27 +2,49 @@
 
 namespace DI\Bridge\Slim;
 
-use Slim\Interfaces\CallableResolverInterface;
+use Slim\Interfaces\AdvancedCallableResolverInterface;
 
 /**
  * Resolve middleware and route callables using PHP-DI.
  */
-class CallableResolver implements CallableResolverInterface
+class CallableResolver implements AdvancedCallableResolverInterface
 {
     /**
      * @var \Invoker\CallableResolver
      */
     private $callableResolver;
+    /**
+     * @var \Slim\CallableResolver
+     */
+    private $slimCallableResolver;
 
-    public function __construct(\Invoker\CallableResolver $callableResolver)
+    public function __construct(\Invoker\CallableResolver $callableResolver, \Slim\CallableResolver $slimCallableResolver)
     {
         $this->callableResolver = $callableResolver;
+        $this->slimCallableResolver = $slimCallableResolver;
     }
+
     /**
      * {@inheritdoc}
      */
     public function resolve($toResolve): callable
     {
         return $this->callableResolver->resolve($toResolve);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveRoute($toResolve): callable
+    {
+        return $this->resolve($toResolve);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveMiddleware($toResolve): callable
+    {
+        return $this->slimCallableResolver->resolveMiddleware($toResolve);
     }
 }

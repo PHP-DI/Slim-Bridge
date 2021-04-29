2c6c3ba5c8d576616e41a6aa914a21a6b8183e64
Andrey Savchenko <contact@rarst.net>
2021-04-29 15:28:49 +0300

    Upgraded CallableResolver to Advanced interface.

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -3,11 +3,12 @@
 namespace DI\Bridge\Slim\Test;
 
 use DI\Bridge\Slim\Bridge;
+use DI\Bridge\Slim\Test\Fixture\UserMiddlewarePsr;
 use DI\Bridge\Slim\Test\Mock\RequestFactory;
+use Laminas\Diactoros\Response\TextResponse;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\TextResponse;
 
 class MiddlewareTest extends TestCase
 {
@@ -20,6 +21,20 @@ class MiddlewareTest extends TestCase
         $app->add(function (ServerRequestInterface $request, RequestHandlerInterface $next) {
             return new TextResponse('Hello ' . $request->getQueryParams()['foo']);
         });
+        $app->get('/', function () {});
+
+        $response = $app->handle(RequestFactory::create('/', 'foo=matt'));
+
+        $this->assertEquals('Hello matt', $response->getBody()->__toString());
+    }
+
+    /**
+     * @test
+     */
+    public function invokes_class_name_middleware()
+    {
+        $app = Bridge::create();
+        $app->add(UserMiddlewarePsr::class);
         $app->get('/', function () {});
 
         $response = $app->handle(RequestFactory::create('/', 'foo=matt'));

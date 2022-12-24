7a93bd0bdf2e649f643af79a1cda6ba19268ad22
Jonathan Nessier <jonathan.nessier@neoflow.ch>
2022-12-24 15:23:12 +0100

    Added support for pre and post calls

--- src/ControllerInvoker.php
+++ src/ControllerInvoker.php
@@ -18,13 +18,14 @@ class ControllerInvoker implements InvocationStrategyInterface
     {
         $this->invoker = $invoker;
     }
+
     /**
      * Invoke a route callable.
      *
-     * @param callable               $callable       The callable to invoke using the strategy.
-     * @param ServerRequestInterface $request        The request object.
-     * @param ResponseInterface      $response       The response object.
-     * @param array                  $routeArguments The route's placeholder arguments
+     * @param callable $callable The callable to invoke using the strategy.
+     * @param ServerRequestInterface $request The request object.
+     * @param ResponseInterface $response The response object.
+     * @param array $routeArguments The route's placeholder arguments
      *
      * @return ResponseInterface|string The response from the callable.
      */
@@ -36,7 +37,7 @@ class ControllerInvoker implements InvocationStrategyInterface
     ): ResponseInterface {
         // Inject the request and response by parameter name
         $parameters = [
-            'request'  => $request,
+            'request' => $request,
             'response' => $response,
         ];
         // Inject the route arguments by name
@@ -44,6 +45,22 @@ class ControllerInvoker implements InvocationStrategyInterface
         // Inject the attributes defined on the request
         $parameters += $request->getAttributes();
 
-        return $this->invoker->call($callable, $parameters);
+        // Check whether callable is class method call
+        $classMethodCall = is_array($callable) && isset($callable[0]) && is_object($callable[0]);
+
+        // Check whether pre action method exists and if true then call it
+        if ($classMethodCall && method_exists($callable[0], 'preCall')) {
+            $parameters['response'] = $this->invoker->call([$callable[0], 'preCall'], $parameters);
+        }
+
+        // Call controller action callable
+        $parameters['response'] = $this->invoker->call($callable, $parameters);
+
+        // Check whether post action method exists and if true then call it
+        if ($classMethodCall && method_exists($callable[0], 'postCall')) {
+            return $this->invoker->call([$callable[0], 'postCall'], $parameters);
+        }
+
+        return $parameters['response'];
     }
 }
